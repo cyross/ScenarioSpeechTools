dd73266403060f8893ac1ff125844cb77b41e038
Cyross Makoto <takuan45189@gmail.com>
2023-01-27 13:19:10 +0900

    ユーティリティ関数の追加

--- utility.py
+++ utility.py
@@ -3,8 +3,15 @@ import os
 import yaml
 import pathlib
 import re
+import shutil
 
 # ユーティリティ関数群
+
+def load_config_file(config_file_path = './config.yaml', encoding = 'utf-8'):
+    config = None
+    with open(config_file_path, 'r', encoding=encoding) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
 
 def sanitarily_actor_name(actor_name):
     return re.sub(r'[:\.\s\(\)\[\]\\\/]+', '_', actor_name)
@@ -22,3 +29,8 @@ def create_voice_engine_dir(engine_name, output_dir):
     output_voice_engine_dir = os.path.join(output_dir, engine_name)
     os.mkdir(output_voice_engine_dir)
 
+def create_serifu_file(filepath):
+    pathlib.Path.touch(filepath)
+
+def copy_file(src_path, dst_path):
+    shutil.copyfile(src_path, dst_path)
